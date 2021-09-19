23f36d8127a8083e64b87244942a73520edbd761
wuchengxing <1449697757@qq.com>
2021-09-19 22:07:00 +0800

    feat：测试栈结构代码

--- src/create-jvm/main.go
+++ src/create-jvm/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"create-jvm/classfile"
 	"create-jvm/classpath"
+	"create-jvm/rtda"
 	"fmt"
-	"strings"
 )
 
 func main() {
 	cmd := parseCmd()
 
 	if cmd.versionFlag {
-		fmt.Println("version 0.0.1")
+		fmt.Println("version 1.0.0")
 	} else if cmd.helpFlag || cmd.class == "" {
 		printUsage()
 	} else {
@@ -20,12 +20,19 @@ func main() {
 }
 
 // 启动vm去加载类
-func startJVM(cmd *Cmd) {
+/*func startJVM(cmd *Cmd) {
 	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	className := strings.Replace(cmd.class, ".", "/", -1)
 	cf := loadClass(className, cp)
 	fmt.Println(cmd.class)
 	printClassInfo(cf)
+}*/
+
+// startJVM // 测试指令
+func startJVM(cmd *Cmd) {
+	frame := rtda.NewFrame(100, 100)
+	testLocalVars(frame.LocalVars())
+	testOperandStack(frame.OperandStack())
 }
 
 /**
@@ -65,4 +72,38 @@ func printClassInfo(cf *classfile.ClassFile) {
 	for _, m := range cf.Methods() {
 		fmt.Printf("  %s\n", m.Name())
 	}
+}
+
+func testLocalVars(vars rtda.LocalVars) {
+	vars.SetInt(0, 100)
+	vars.SetInt(1, -100)
+	vars.SetLong(2, 2997924580)
+	vars.SetLong(4, -2997924580)
+	vars.SetFloat(6, 3.1415926)
+	vars.SetDouble(7, 2.71828182845)
+	vars.SetRef(9, nil)
+	println(vars.GetInt(0))
+	println(vars.GetInt(1))
+	println(vars.GetLong(2))
+	println(vars.GetLong(4))
+	println(vars.GetFloat(6))
+	println(vars.GetDouble(7))
+	println(vars.GetRef(9))
+}
+
+func testOperandStack(ops *rtda.OperandStack) {
+	ops.PushInt(100)
+	ops.PushInt(-100)
+	ops.PushLong(2997924580)
+	ops.PushLong(-2997924580)
+	ops.PushFloat(3.1415926)
+	ops.PushDouble(2.71828182845)
+	ops.PushRef(nil)
+	println(ops.PopRef())
+	println(ops.PopDouble())
+	println(ops.PopFloat())
+	println(ops.PopLong())
+	println(ops.PopLong())
+	println(ops.PopInt())
+	println(ops.PopInt())
 }
